c2fc78895d00d024b1774163e93a77d231fa6d60
Fann Lee <57746626+Stardust-lf@users.noreply.github.com>
2024-12-21 12:59:51 -0500

    Delete _deps directory

--- _deps/yaml-cpp-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fan/projects/ramulator2-RWfrontend/ext/yaml-cpp
-# Build directory: /home/fan/projects/ramulator2-RWfrontend/_deps/yaml-cpp-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
